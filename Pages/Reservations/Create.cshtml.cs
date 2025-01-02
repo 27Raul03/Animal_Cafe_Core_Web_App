7a609cf8c226fd7145b7e80d65036619d0aab7dc
Melisa <mocan.melisa.maria@gmail.com>
2025-01-02 18:06:19 +0200

    all done

--- Pages/Reservations/Create.cshtml.cs
+++ Pages/Reservations/Create.cshtml.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Animal_Cafe_Core_Web_App.Data;
 using Animal_Cafe_Core_Web_App.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
 
 namespace Animal_Cafe_Core_Web_App.Pages.Reservations
 {
@@ -24,7 +25,7 @@ namespace Animal_Cafe_Core_Web_App.Pages.Reservations
 
         public IActionResult OnGet()
         {
-        ViewData["ClientID"] = new SelectList(_context.Client, "ID", "Name");
+        ViewData["ClientID"] = new SelectList(_context.Client, "ID", "Email");
         ViewData["AnimalID"] = new SelectList(_context.Animal, "ID", "Name");
             return Page();
         }
@@ -35,6 +36,22 @@ namespace Animal_Cafe_Core_Web_App.Pages.Reservations
         // For more information, see https://aka.ms/RazorPagesCRUD.
         public async Task<IActionResult> OnPostAsync()
         {
+            var loggedInUserEmail = User.Identity?.Name;
+
+            var selectedClientEmail = await _context.Client
+                .Where(c => c.ID == Reservation.ClientID)
+                .Select(c => c.Email)
+                .FirstOrDefaultAsync();
+
+            if (!string.Equals(loggedInUserEmail, selectedClientEmail, StringComparison.OrdinalIgnoreCase))
+            {
+                ModelState.AddModelError("Reservation.ClientID", "Selected client email does not match your logged-in email.");
+
+                ViewData["AnimalID"] = new SelectList(_context.Animal, "ID", "Name");
+                ViewData["ClientID"] = new SelectList(_context.Client, "ID", "Email");
+
+                return Page();
+            }
 
             _context.Reservation.Add(Reservation);
             await _context.SaveChangesAsync();
